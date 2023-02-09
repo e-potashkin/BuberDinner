9a2fb2bb61332b307d5bbc7e24c0aed87568a7ac
Eugene Potashkin <yevhenii.potashkin@arvosoftware.com>
2023-02-09 14:52:39 +0200

    replace static logger in LoggingBehavior

--- src/BuberDinner.Application/Common/Behaviors/LoggingBehavior.cs
+++ src/BuberDinner.Application/Common/Behaviors/LoggingBehavior.cs
@@ -1,6 +1,6 @@
 using ErrorOr;
 using MediatR;
-using Serilog;
+using Microsoft.Extensions.Logging;
 
 namespace BuberDinner.Application.Common.Behaviors;
 
@@ -8,12 +8,19 @@ public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, 
     where TRequest : IRequest<TResponse>
     where TResponse : IErrorOr
 {
+    private readonly ILogger<ValidationBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(ILogger<ValidationBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var result = await next();
         if (result.IsError)
         {
-            Log.Error("Request failure {Name}, Errors: {@Errors}", typeof(TRequest).Name, result.Errors);
+            _logger.LogError("Request failure {Name}, Errors: {@Errors}", typeof(TRequest).Name, result.Errors);
         }
 
         return result;
