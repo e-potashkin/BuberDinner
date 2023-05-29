bf3f357d5e45909a3fbd3ddf2e3e6ed314895007
Eugene Potashkin <yevhenii.potashkin@arvosoftware.com>
2023-05-29 15:20:55 +0300

    New: health checks

--- src/Modules/BuberDinner/BuberDinner.Api/DependencyInjection.cs
+++ src/Modules/BuberDinner/BuberDinner.Api/DependencyInjection.cs
@@ -8,13 +8,15 @@ namespace BuberDinner.Api;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    public static IServiceCollection AddPresentation(this IServiceCollection services, ConfigurationManager configurationManager)
     {
         services.AddControllers();
         services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
         services.AddMappings();
         services.AddRateLimiter();
         services.AddOutputCache();
+        services.AddHealthChecks()
+            .AddNpgSql(configurationManager["PostgresOptions:ConnectionString"]!);
 
         return services;
     }
--- src/Modules/BuberDinner/BuberDinner.Api/Program.cs
+++ src/Modules/BuberDinner/BuberDinner.Api/Program.cs
@@ -2,13 +2,15 @@ using BuberDinner.Api;
 using BuberDinner.Api.Common.Configurations;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(LoggingConfiguration.ConfigureLogger);
 builder.Services
-    .AddPresentation()
+    .AddPresentation(builder.Configuration)
     .AddApplication()
     .AddInfrastructure(builder.Environment.IsDevelopment());
 
@@ -18,6 +20,10 @@ app.UseRateLimiter();
 app.UseExceptionHandler("/error");
 app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
+app.MapHealthChecks("/_health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 app.MapControllers();
 app.UseOutputCache();
 
