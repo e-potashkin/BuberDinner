3bec6d5fd380dda3db471097901509989074786f
Eugene Potashkin <yevhenii.potashkin@arvosoftware.com>
2023-07-01 07:45:11 +0300

    Add Validation and Logging behaviors to MediatR
    In this revision, two behavioral changes were made to the MediatR configuration in the BuberDinner module. The ValidationBehavior and LoggingBehavior were moved from being transient services and added directly in the MediatR configuration. This change was necessary to improve the application's performance and maintainability by reducing unnecessary redundancy and facilitating easier behavioral modifications in future.

--- src/Modules/BuberDinner/BuberDinner.Application/DependencyInjection.cs
+++ src/Modules/BuberDinner/BuberDinner.Application/DependencyInjection.cs
@@ -11,10 +11,14 @@ public static class DependencyInjection
     {
         var assembly = typeof(DependencyInjection).Assembly;
 
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
         services.AddValidatorsFromAssembly(assembly, ServiceLifetime.Singleton);
-        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
 
         return services;
     }
